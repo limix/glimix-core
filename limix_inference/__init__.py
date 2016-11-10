f682ae1bfadd26cc12ee62308a2b3fd5e372177e
Danilo Horta <danilo.horta@gmail.com>
2016-11-10 22:22:13 +0000

    saving

--- limix_inference/__init__.py
+++ limix_inference/__init__.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import as _absolute_import
+
 from .fastlmm import FastLMM
 from .slowlmm import SlowLMM
 from .ep import ExpFamEP
@@ -6,3 +8,29 @@ from . import lik
 from . import mean
 from . import link
 from . import random
+
+from pkg_resources import get_distribution as _get_distribution
+from pkg_resources import DistributionNotFound as _DistributionNotFound
+
+try:
+    __version__ = _get_distribution('limix_inference').version
+except _DistributionNotFound:
+    __version__ = 'unknown'
+
+
+def test():
+    import os
+    p = __import__('limix_inference').__path__[0]
+    src_path = os.path.abspath(p)
+    old_path = os.getcwd()
+    os.chdir(src_path)
+
+    try:
+        return_code = __import__('pytest').main(['-q'])
+    finally:
+        os.chdir(old_path)
+
+    if return_code == 0:
+        print("Congratulations. All tests have passed!")
+
+    return return_code
