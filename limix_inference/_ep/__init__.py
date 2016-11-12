c9fa98ceed6a690c3d97aeafb6894f047f76f548
Danilo Horta <danilo.horta@gmail.com>
2016-11-12 09:50:38 +0000

    ok

--- limix_inference/_ep/__init__.py
+++ limix_inference/_ep/__init__.py
@@ -7,11 +7,17 @@ Example:
 .. doctest::
 
     >>> from limix_inference.random import bernoulli_sample
+    >>> from limix_inference import ExpFamEP
     >>> from numpy.random import RandomState
     >>> offset = 5
     >>> G = [[1, -1], [2, 1]]
-    >>> bernoulli_sample(offset, G, random_state=RandomState(0))
-    array([1, 1])
+    >>> (Q0, Q1), S0 = econommic_qs_linear(G)
+    >>> y = bernoulli_sample(offset, G, random_state=RandomState(0))
+    >>> covariantes = [1., 0.6]
+    >>> glmm = ExpFamEP(BernoulliProdLik(Logit), covariantes, Q0, Q1, S0)
+    >>> glmm.optmize()
+    >>> glmm.lml()
+    23.3
 
 ExpFamEP
 ^^^^^^^^
--- limix_inference/random/_canonical.py
+++ limix_inference/random/_canonical.py
@@ -40,6 +40,21 @@ def bernoulli_sample(offset,
         ~ (h^2 - v_c)\mathrm G^\intercal\mathrm G +
         (1-h^2-v_c)\mathrm I ~)
 
+    using the canonical Logit link function to define the conditional Bernoulli
+    mean :math:`\mu_i`.
+
+    The causal :math:`\mathbf a` covariates and the corresponding effect-sizes
+    are randomly draw according to the following idea. The ``causal_variants``,
+    if given, are first mean-zero and std-one normalized and then having
+    its elements divided by the squared-root the the number of variances::
+
+        causal_variants = _stdnorm(causal_variants, axis=0)
+        causal_variants /= sqrt(causal_variants.shape[1])
+
+    The causal effect-sizes :math:`\boldsymbol\alpha` are draw from
+    :math:`\{-1, +1\}` and subsequently normalized for mean-zero and std-one""
+
+
     """
     link = LogitLink()
     mean, cov = _mean_cov(offset, G, heritability, causal_variants,
