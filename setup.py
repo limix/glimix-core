0e9d0bf69f105db5a74e5653b2d9a351b83ea202
Danilo Horta <danilo.horta@gmail.com>
2016-12-15 09:54:25 +0000

    1.0.2 release

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ def setup_package():
 
     metadata = dict(
         name='limix-inference',
-        version='1.0.2rc3',
+        version='1.0.2',
         maintainer="Limix Developers",
         maintainer_email="horta@ebi.ac.uk",
         license="MIT",
