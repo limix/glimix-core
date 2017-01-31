6e27185679035b7436d080e59cc5ef76d9dda152
Danilo Horta <danilo.horta@gmail.com>
2017-01-31 10:24:44 +0000

    automatic

--- limix_inference/cov/test/test_eye_cov.py
+++ limix_inference/cov/test/test_eye_cov.py
@@ -40,40 +40,21 @@ def test_eye_gradient_1():
     assert_allclose(check_grad(func, grad, [0.1]), 0, atol=1e-7)
 
 
-# def test_eye_gradient_2():
-#     cov = EyeCov()
-#     cov.scale = 2.1
-#     a = arange(5)
-#     cov.set_data((a, a))
-#
-#     def func(x):
-#         cov.scale = exp(x[0])
-#         return cov.feed().value()
-#
-#     def grad(x):
-#         cov.scale = exp(x[0])
-#         return cov.feed().gradient()
-#
-#     assert_allclose(check_grad(func, grad, [0.1]), 0, atol=1e-7)
+def test_eye_gradient_2():
+    cov = EyeCov()
+    cov.scale = 2.1
+    a = arange(5)
+    cov.set_data((a, a))
 
+    def func(x):
+        cov.scale = exp(x[0])
+        return cov.feed().value()
 
-# def test_eye_gradient_3():
-#     cov = EyeCov()
-#     cov.scale = 2.1
-#     a = Apples(5)
-#     o = Oranges(4)
-#     cov.set_data((a, o))
-#
-#     def func(x):
-#         cov.scale = exp(x[0])
-#         return cov.feed().value()
-#
-#     def grad(x):
-#         cov.scale = exp(x[0])
-#         return cov.feed().gradient()
-#
-#     assert_allclose(check_grad(func, grad, [0.1]), 0, atol=1e-7)
+    def grad(x):
+        cov.scale = exp(x[0])
+        return cov.feed().gradient()
 
+    assert_allclose(check_grad(func, grad, [0.1]), 0, atol=1e-7)
 
 if __name__ == '__main__':
     __import__('pytest').main([__file__, '-s'])
