217439f7c55ba9eb6ec923004957151eef0501da
Danilo Horta <danilo.horta@gmail.com>
2017-03-08 11:44:17 +0000

    doctest

--- limix_inference/__init__.py
+++ limix_inference/__init__.py
@@ -25,7 +25,7 @@ def test():
     os.chdir(src_path)
 
     try:
-        return_code = __import__('pytest').main(['-q'])
+        return_code = __import__('pytest').main(['-a', '--doctest-modules'])
     finally:
         os.chdir(old_path)
 
