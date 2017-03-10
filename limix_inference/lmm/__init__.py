6e56de7f841b0024bd75a4d51737a184d531cf92
Danilo Horta <danilo.horta@gmail.com>
2017-03-10 13:56:02 +0000

    Introduction

--- limix_inference/lmm/__init__.py
+++ limix_inference/lmm/__init__.py
@@ -1,9 +1,26 @@
-"""
+r"""
 *******************
 Linear Mixed Models
 *******************
 
-We have two implementations: :class:`.SlowLMM` and :class:`.FastLMM`.
+Introduction
+^^^^^^^^^^^^
+
+A LMM can be described as
+
+.. math::
+
+    \mathbf y = \mathrm M\boldsymbol\beta + \mathbf u + \boldsymbol\epsilon
+
+where :math:`\mathbf u \sim \mathcal N(\mathbf 0, \mathrm K)` and
+:math:`\epsilon_i` are iid Normal random variables.
+This module provides two methods for fitting LMMs via maximum
+likelihood: :class:`.SlowLMM` and :class:`.FastLMM`.
+The former is more general but slower then the later;
+the later one assumes a scaled covariance matrix.
+
+Public interface
+^^^^^^^^^^^^^^^^
 """
 
 from .fastlmm import FastLMM, NormalLikTrick
