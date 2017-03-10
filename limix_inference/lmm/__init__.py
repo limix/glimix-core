bef2e0d8ec5662740d08e12c40df3ead80918040
Danilo Horta <danilo.horta@gmail.com>
2017-03-10 11:16:09 +0000

    we have two

--- limix_inference/lmm/__init__.py
+++ limix_inference/lmm/__init__.py
@@ -3,24 +3,7 @@
 Linear Mixed Models
 *******************
 
-- :class:`.FastLMM`
-- :class:`.SlowLMM`
-- :class:`.NormalLikTrick`
-
-Fast implementation
-^^^^^^^^^^^^^^^^^^^
-
-.. autoclass:: FastLMM
-  :members:
-
-.. autoclass:: NormalLikTrick
-  :members:
-
-General implementation
-^^^^^^^^^^^^^^^^^^^^^^
-
-.. autoclass:: SlowLMM
-  :members:
+We have two implementations: :class:`.SlowLMM` and :class:`.FastLMM`.
 """
 
 from .fastlmm import FastLMM, NormalLikTrick
