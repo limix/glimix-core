4992a5871a2c192979657ef0f6300756d32d9efe
Danilo Horta <danilo.horta@gmail.com>
2017-03-10 10:06:50 +0000

    doc

--- limix_inference/__init__.py
+++ limix_inference/__init__.py
@@ -1,3 +1,47 @@
+"""
+***********************
+limix_inference package
+***********************
+
+Example:
+
+.. doctest::
+
+    >>> from numpy import arange, sqrt
+    >>> from numpy.random import RandomState
+    >>> from limix_inference.random import GLMMSampler
+    >>> from limix_inference.mean import OffsetMean
+    >>> from limix_inference.cov import LinearCov, EyeCov, SumCov
+    >>> from limix_inference.lik import DeltaProdLik
+    >>>
+    >>> random = RandomState(9458)
+    >>> N = 500
+    >>> X = random.randn(N, N + 1)
+    >>> X -= X.mean(0)
+    >>> X /= X.std(0)
+    >>> X /= sqrt(X.shape[1])
+    >>> offset = 1.0
+    >>>
+    >>> mean = OffsetMean()
+    >>> mean.offset = offset
+    >>> mean.set_data(N, purpose='sample')
+    >>>
+    >>> cov_left = LinearCov()
+    >>> cov_left.scale = 1.5
+    >>> cov_left.set_data((X, X), purpose='sample')
+    >>>
+    >>> cov_right = EyeCov()
+    >>> cov_right.scale = 1.5
+    >>> cov_right.set_data((arange(N), arange(N)), purpose='sample')
+    >>>
+    >>> cov = SumCov([cov_left, cov_right])
+    >>>
+    >>> lik = DeltaProdLik()
+    >>>
+    >>> y = GLMMSampler(lik, mean, cov).sample(random)
+    >>> print(y[:5])
+
+"""
 from __future__ import absolute_import as _absolute_import
 
 from . import lmm
@@ -25,7 +69,7 @@ def test():
     os.chdir(src_path)
 
     try:
-        return_code = __import__('pytest').main(['-a', '--doctest-modules'])
+        return_code = __import__('pytest').main(['-q', '--doctest-modules'])
     finally:
         os.chdir(old_path)
 
--- limix_inference/lik/__init__.py
+++ limix_inference/lik/__init__.py
@@ -1,3 +1,31 @@
+"""
+***********
+Likelihoods
+***********
+
+- :class:`.LinearMean`
+- :class:`.OffsetMean`
+- :class:`.SumMean`
+
+Linear mean
+^^^^^^^^^^^
+
+.. autoclass:: LinearMean
+  :members:
+
+Offset mean
+^^^^^^^^^^^
+
+.. autoclass:: OffsetMean
+  :members:
+
+Sum mean
+^^^^^^^^
+
+.. autoclass:: SumMean
+  :members:
+"""
+
 from ._expfam import (DeltaLik, BernoulliLik, BinomialLik, PoissonLik)
 from ._prod import (DeltaProdLik, BernoulliProdLik, BinomialProdLik,
                     PoissonProdLik)
