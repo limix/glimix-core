618b3193196fe84abc173b6932356cd8190003be
Danilo Horta <danilo.horta@gmail.com>
2017-03-11 16:45:13 +0000

    LMM and GLMM doc

--- limix_inference/glmm/__init__.py
+++ limix_inference/glmm/__init__.py
@@ -7,25 +7,34 @@ Introduction
 ^^^^^^^^^^^^
 
 A GLMM can be described in two parts.
-The first part consists in latent effects:
+The first part consists in the latent variable
 
 .. math::
 
-    \mathbf z = \mathbf m + \mathbf u + \boldsymbol\epsilon
+    \mathbf z = \mathrm X\boldsymbol\beta + \mathrm Z\mathbf u,
 
-where :math:`\mathbf u \sim \mathcal N(\mathbf 0, \mathrm K)` and
-:math:`\epsilon_i` are iid Normal random variables.
+where :math:`\mathbf u \sim \mathcal N(\mathbf 0, \mathrm G)` is a vector of
+random effects.
 The second part connects the latent variable to the observed one:
 
 .. math::
 
-    y_i ~|~ z_i \sim \text{ExpFam}(y_i ~|~ \mu_i = g(z_i))
+    y_i ~|~ z_i \sim \text{ExpFam}(\mu_i = g(z_i)),
 
 where :math:`g(\cdot)` is a link function and :math:`\text{ExpFam}(\cdot)` is
 an exponential-family distribution.
+The marginal likelihood is thus given by
 
-This module implements the Expectation Propagation algorithm:
-:class:`.ExpFamEP`.
+.. math::
+
+    p(\mathbf y) = \int
+      \prod_i \text{ExpFam}(y_i ~|~ \mu_i = g(z_i))
+      \mathcal N(\mathbf z ~|~ \mathrm X\boldsymbol\beta,
+                 \mathrm Z \mathrm G \mathrm Z^{\intercal})
+    d\mathbf z
+
+This module implements the Expectation Propagation algorithm for parameter
+fitting via Maximum Likelihood: :class:`.ExpFamEP`.
 
 Public interface
 ^^^^^^^^^^^^^^^^
--- limix_inference/lmm/__init__.py
+++ limix_inference/lmm/__init__.py
@@ -10,10 +10,20 @@ A LMM can be described as
 
 .. math::
 
-    \mathbf y = \mathbf m + \mathbf u + \boldsymbol\epsilon
+    \mathbf y = \mathrm X\boldsymbol\beta + \mathrm Z\mathbf u
+    + \boldsymbol\epsilon,
 
-where :math:`\mathbf u \sim \mathcal N(\mathbf 0, \mathrm K)` and
-:math:`\epsilon_i` are iid Normal random variables.
+where :math:`\mathbf u \sim \mathcal N(\mathbf 0, \mathrm G)` is a vector of
+random effects and :math:`\epsilon_i` are iid Normal random variables with
+zero-mean and variance :math:`\sigma_{\epsilon}^2` each.
+The outcome-vector is thus distributed according to
+
+.. math::
+
+    \mathbf y \sim \mathcal N(\mathrm X\boldsymbol\beta,
+                              \mathrm Z \mathrm G \mathrm Z^{\intercal}
+                              + \sigma_{\epsilon}^2\mathrm I)
+
 This module provides two methods for fitting LMMs via maximum
 likelihood: :class:`.SlowLMM` and :class:`.FastLMM`.
 The former is more general but slower than the latter;
