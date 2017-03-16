8dd5dba55ae159743ce64133dc77086b68098bea
Danilo Horta <danilo.horta@gmail.com>
2017-03-16 15:57:07 +0000

    lmm clean up

--- limix_inference/lmm/__init__.py
+++ limix_inference/lmm/__init__.py
@@ -10,28 +10,24 @@ A LMM can be described as
 
 .. math::
 
-    \mathbf y = \mathrm X\boldsymbol\beta + \mathrm Z\mathbf u
+    \mathbf y = \mathrm X\boldsymbol\beta + \mathrm G\mathbf u
     + \boldsymbol\epsilon,
 
-where :math:`\mathbf u \sim \mathcal N(\mathbf 0, \mathrm G)` is a vector of
-random effects and :math:`\epsilon_i` are iid Normal random variables with
-zero-mean and variance :math:`\sigma_{\epsilon}^2` each.
+where :math:`\mathbf u \sim \mathcal N(\mathbf 0, \sigma_r^2\mathrm I)` is a
+vector of random effects and :math:`\epsilon_i` are iid Normal random variables
+with zero-mean and variance :math:`\sigma_e^2` each.
 The outcome-vector is thus distributed according to
 
 .. math::
 
     \mathbf y \sim \mathcal N(\mathrm X\boldsymbol\beta,
-                              \mathrm Z \mathrm G \mathrm Z^{\intercal}
-                              + \sigma_{\epsilon}^2\mathrm I)
+                              \sigma_r^2 \mathrm G \mathrm G^{\intercal}
+                              + \sigma_e^2\mathrm I)
 
-This module provides two methods for fitting LMMs via maximum
-likelihood: :class:`.SlowLMM` and :class:`.LMM`.
-The former is more general but slower than the latter;
-the latter assumes a scaled covariance matrix and linear fixed-effects.
-
+This module provides a FastLMM implementation :class:`.LMM`
+to perform inference over the parameters.
 :class:`.FastScanner` can be used to perform fast inference over multiple
-covariates;
-it is meant to perform such inference over millions of covariates in seconds.
+covariates.
 
 Public interface
 ^^^^^^^^^^^^^^^^
--- limix_inference/lmm/core.py
+++ limix_inference/lmm/core.py
@@ -1,11 +1,12 @@
 from __future__ import division
 
+from copy import copy
+
 from numpy import (ascontiguousarray, dot, log, sqrt, var, zeros)
 from numpy.random import RandomState
-from scipy.stats import multivariate_normal
 
 from numpy_sugar import epsilon
-from numpy_sugar.linalg import ddot, economic_svd, solve, sum2diag
+from numpy_sugar.linalg import ddot, economic_svd, solve
 
 from .scan import FastScanner
 
@@ -33,8 +34,9 @@ class LMMCore(object):
 
         self._n = self._y.shape[0]
         self._p = self._n - QS[1].shape[0]
-        self._diag0 = QS[1] * 0.5 + 0.5
-        self._diag1 = 0.5
+
+        self._diag = [QS[1] * 0.5 + 0.5, 0.5]
+
         self._tM = None
         self.__tbeta = None
         self._covariate_setup(M)
@@ -45,26 +47,19 @@ class LMMCore(object):
         self._delta = 0.5
         self._lml = 0.0
 
-        self._a1 = 0.0
-        self._b1 = zeros(d)
-        self._c1 = zeros((d, d))
+        self._a = [0.0, 0.0]
 
-        self._a0 = 0.0
-        self._b0 = zeros(d)
-        self._c0 = zeros((d, d))
+        self._b = [zeros(d), zeros(d)]
+        self._c = [zeros((d, d)), zeros((d, d))]
 
-        self._yTQ0 = dot(self._y.T, QS[0][0])
-        self._yTQ0_2x = self._yTQ0**2
+        self._yTQ = [dot(self._y.T, QS[0][i]) for i in [0, 1]]
 
-        self._yTQ1 = dot(self._y.T, QS[0][1])
-        self._yTQ1_2x = self._yTQ1**2
+        self._yTQ_2x = [self._yTQ[i]**2 for i in [0, 1]]
 
-        self._tMTQ0 = self._tM.T.dot(QS[0][0])
-        self._tMTQ1 = self._tM.T.dot(QS[0][1])
+        self._tMTQ = [self._tM.T.dot(QS[0][i]) for i in [0, 1]]
 
         self.valid_update = False
-        self.__Q0tymD0 = None
-        self.__Q1tymD1 = None
+        # self.__QtymD = [None, None]
 
     def _covariate_setup(self, M):
         SVD = economic_svd(M)
@@ -83,8 +78,7 @@ class LMMCore(object):
         o._fix_scale = self._fix_scale
         o._n = self._n
         o._p = self._p
-        o._diag0 = self._diag0.copy()
-        o._diag1 = self._diag1
+        o._diag = [copy(self._diag[i]) for i in [0, 1]]
         o._QS = self._QS
         o._M = self._M
         o._y = self._y
@@ -94,27 +88,16 @@ class LMMCore(object):
         o._delta = self._delta
         o._lml = self._lml
 
-        o._a1 = self._a1
-        o._b1 = self._b1.copy()
-        o._c1 = self._c1.copy()
+        o._a = [copy(self._a[i]) for i in [0, 1]]
+        o._b = [copy(self._b[i]) for i in [0, 1]]
+        o._c = [copy(self._c[i]) for i in [0, 1]]
 
-        o._a0 = self._a0
-        o._b0 = self._b0.copy()
-        o._c0 = self._c0.copy()
-
-        o._yTQ0 = self._yTQ0
-        o._yTQ0_2x = self._yTQ0_2x
-
-        o._yTQ1 = self._yTQ1
-        o._yTQ1_2x = self._yTQ1_2x
-
-        o._tMTQ0 = self._tMTQ0
-        o._tMTQ1 = self._tMTQ1
+        o._yTQ = self._yTQ
+        o._yTQ_2x = self._yTQ_2x
+        o._yTQ = self._yTQ
+        o._tMTQ = self._tMTQ
 
         o.valid_update = self.valid_update
-        from copy import copy
-        o.__Q0tymD0 = copy(self.__Q0tymD0)
-        o.__Q1tymD1 = copy(self.__Q1tymD1)
 
         return o
 
@@ -135,35 +118,37 @@ class LMMCore(object):
         d = self._tM.shape[1]
         self.__tbeta = zeros(d)
 
-        self._b1 = zeros(d)
-        self._c1 = zeros((d, d))
+        self._b = [zeros(d), zeros(d)]
+        self._c = [zeros((d, d)), zeros((d, d))]
 
-        self._b0 = zeros(d)
-        self._c0 = zeros((d, d))
-
-        self._tMTQ0 = self._tM.T.dot(self._QS[0][0])
-        self._tMTQ1 = self._tM.T.dot(self._QS[0][1])
+        self._tMTQ = [self._tM.T.dot(self._QS[0][i]) for i in [0, 1]]
 
         self.valid_update = 0
-        self.__Q0tymD0 = None
-        self.__Q1tymD1 = None
+        # self.__Q0tymD0 = None
+        # self.__Q1tymD1 = None
 
     @property
     def m(self):
         r"""Returns :math:`\mathbf m = \mathrm M \boldsymbol\beta`."""
         return dot(self._tM, self._tbeta)
 
-    def _Q0tymD0(self):
-        if self.__Q0tymD0 is None:
-            Q0tym = self._yTQ0 - self.__tbeta.dot(self._tMTQ0)
-            self.__Q0tymD0 = Q0tym / self._diag0
-        return self.__Q0tymD0
+    # def _QtymD(self, i):
+    #     if self.__QtymD[i] is None:
+    #         Qtym[i] = self._yTQ[i] - self.__tbeta.dot(self._tMTQ[i])
+    #         self.__QtymD[i] = Qtym[ii] / self._diag[i]
+    #     return self.__QtymD[i]
 
-    def _Q1tymD1(self):
-        if self.__Q1tymD1 is None:
-            Q1tym = self._yTQ1 - self.__tbeta.dot(self._tMTQ1)
-            self.__Q1tymD1 = Q1tym / self._diag1
-        return self.__Q1tymD1
+    # def _Q0tymD0(self):
+    #     if self.__Q0tymD0 is None:
+    #         Q0tym = self._yTQ0 - self.__tbeta.dot(self._tMTQ0)
+    #         self.__Q0tymD0 = Q0tym / self._diag0
+    #     return self.__Q0tymD0
+    #
+    # def _Q1tymD1(self):
+    #     if self.__Q1tymD1 is None:
+    #         Q1tym = self._yTQ1 - self.__tbeta.dot(self._tMTQ1)
+    #         self.__Q1tymD1 = Q1tym / self._diag1
+    #     return self.__Q1tymD1
 
     @property
     def _tbeta(self):
@@ -196,40 +181,35 @@ class LMMCore(object):
     @delta.setter
     def delta(self, delta):
         self.valid_update = False
-        self.__Q0tymD0 = None
-        self.__Q1tymD1 = None
+        # self.__QtymD = [None, None]
         self._delta = delta
 
     def _update_joints(self):
-        yTQ0_2x = self._yTQ0_2x
-        yTQ1_2x = self._yTQ1_2x
+        for i in [0, 1]:
+            self._a[i] = sum(self._yTQ_2x[i] / self._diag[i])
+            self._b[i][:] = (self._yTQ[i] / self._diag[i]).dot(self._tMTQ[i].T)
+            self._c[i][:] = (self._tMTQ[i] / self._diag[i]).dot(self._tMTQ[i].T)
 
-        self._a1 = yTQ1_2x.sum() / self._diag1
-        self._b1[:] = (self._yTQ1 / self._diag1).dot(self._tMTQ1.T)
-        self._c1[:] = (self._tMTQ1 / self._diag1).dot(self._tMTQ1.T)
-
-        self._a0 = (yTQ0_2x / self._diag0).sum()
-        self._b0[:] = (self._yTQ0 / self._diag0).dot(self._tMTQ0.T)
-        self._c0[:] = (self._tMTQ0 / self._diag0).dot(self._tMTQ0.T)
 
     def _update_fixed_effects(self):
-        nominator = self._b1 - self._b0
-        denominator = self._c1 - self._c0
+        nominator = self._b[1] - self._b[0]
+        denominator = self._c[1] - self._c[0]
         self._tbeta = solve(denominator, nominator)
 
     def _update_scale(self):
         if self._fix_scale:
             return
+        a = self._a
         b = self.__tbeta
-        p0 = self._a1 - 2 * self._b1.dot(b) + b.dot(self._c1.dot(b))
-        p1 = self._a0 - 2 * self._b0.dot(b) + b.dot(self._c0).dot(b)
-        self._scale = (p0 + p1) / self._n
+        c = self._c
+        p = [a[i] - 2 * self._b[i].dot(b) + b.dot(c[i]).dot(b) for i in [0, 1]]
+        self._scale = sum(p) / self._n
 
     def _update_diags(self):
-        self._diag0[:] = self._QS[1]
-        self._diag0 *= (1 - self._delta)
-        self._diag0 += self._delta
-        self._diag1 = self._delta
+        self._diag[0][:] = self._QS[1]
+        self._diag[0] *= (1 - self._delta)
+        self._diag[0] += self._delta
+        self._diag[1] = self._delta
 
     def update(self):
         if self.valid_update:
@@ -252,47 +232,47 @@ class LMMCore(object):
         p = self._p
         LOG2PI = 1.837877066409345339081937709124758839607238769531250
         self._lml = -n * LOG2PI - n - n * log(self._scale)
-        self._lml += -sum(log(self._diag0)) - p * log(self._diag1)
+        self._lml += -sum(log(self._diag[0])) - p * log(self._diag[1])
         self._lml /= 2
         return self._lml
 
-    def predict(self, covariates, Cp, Cpp):
-        delta = self.delta
+    # def predict(self, covariates, Cp, Cpp):
+    #     delta = self.delta
+    #
+    #     diag0 = self._diag0
+    #     diag1 = self._diag1
+    #
+    #     CpQ0 = Cp.dot(self._QS[0][0])
+    #     CpQ1 = Cp.dot(self._QS[0][1])
+    #
+    #     m = covariates.dot(self.beta)
+    #     mean = m + (1 - delta) * CpQ0.dot(self._Q0tymD0())
+    #     mean += (1 - delta) * CpQ1.dot(self._Q1tymD1())
+    #
+    #     cov = sum2diag(Cpp * (1 - self.delta), self.delta)
+    #     cov -= (1 - delta)**2 * CpQ0.dot((CpQ0 / diag0).T)
+    #     cov -= (1 - delta)**2 * CpQ1.dot((CpQ1 / diag1).T)
+    #     cov *= self.scale
+    #
+    #     return LMMPredictor(mean, cov)
 
-        diag0 = self._diag0
-        diag1 = self._diag1
 
-        CpQ0 = Cp.dot(self._QS[0][0])
-        CpQ1 = Cp.dot(self._QS[0][1])
-
-        m = covariates.dot(self.beta)
-        mean = m + (1 - delta) * CpQ0.dot(self._Q0tymD0())
-        mean += (1 - delta) * CpQ1.dot(self._Q1tymD1())
-
-        cov = sum2diag(Cpp * (1 - self.delta), self.delta)
-        cov -= (1 - delta)**2 * CpQ0.dot((CpQ0 / diag0).T)
-        cov -= (1 - delta)**2 * CpQ1.dot((CpQ1 / diag1).T)
-        cov *= self.scale
-
-        return LMMPredictor(mean, cov)
-
-
-class LMMPredictor(object):
-    def __init__(self, mean, cov):
-        self._mean = mean
-        self._cov = cov
-        self._mvn = multivariate_normal(mean, cov)
-
-    @property
-    def mean(self):
-        return self._mean
-
-    @property
-    def cov(self):
-        return self._cov
-
-    def pdf(self, y):
-        return self._mvn.pdf(y)
-
-    def logpdf(self, y):
-        return self._mvn.logpdf(y)
+# class LMMPredictor(object):
+#     def __init__(self, mean, cov):
+#         self._mean = mean
+#         self._cov = cov
+#         self._mvn = multivariate_normal(mean, cov)
+#
+#     @property
+#     def mean(self):
+#         return self._mean
+#
+#     @property
+#     def cov(self):
+#         return self._cov
+#
+#     def pdf(self, y):
+#         return self._mvn.pdf(y)
+#
+#     def logpdf(self, y):
+#         return self._mvn.logpdf(y)
