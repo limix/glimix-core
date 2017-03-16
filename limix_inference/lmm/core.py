5fb51b2b92f40b2e7fab2a08dc7dea1e1b435760
Danilo Horta <danilo.horta@gmail.com>
2017-03-16 16:42:07 +0000

    no diag

--- limix_inference/lmm/core.py
+++ limix_inference/lmm/core.py
@@ -1,7 +1,5 @@
 from __future__ import division
 
-from copy import copy
-
 from numpy import ascontiguousarray, dot, log, sqrt, var, zeros
 from numpy.random import RandomState
 from numpy_sugar import epsilon
@@ -31,34 +29,14 @@ class LMMCore(object):
 
         self._fix_scale = False
 
-        self._diag = [QS[1] * 0.5 + 0.5, 0.5]
-
         self._tM = None
         self.__tbeta = None
-        # self._covariate_setup(M)
 
         self._svd = None
         self.M = M
-        # self._svd = economic_svd(M)
-        # self._tM = ddot(self._svd[0], sqrt(self._svd[1]), left=False)
-        # self.__tbeta = zeros(self._tM.shape[1])
-
-        # self._M = M
 
         self._scale = 1.0
         self._delta = 0.5
-
-        # self._yTQ = [dot(self._y.T, QS[0][i]) for i in [0, 1]]
-        # self._yTQ_2x = [self._yTQ[i]**2 for i in [0, 1]]
-        # self._tMTQ = [self._tM.T.dot(QS[0][i]) for i in [0, 1]]
-
-    # def _covariate_setup(self, M):
-    #     SVD = economic_svd(M)
-    #     self._svd_U = SVD[0]
-    #     self._svd_S12 = sqrt(SVD[1])
-    #     self._svd_V = SVD[2]
-    #     self._tM = ddot(self._svd_U, self._svd_S12, left=False)
-    #     self.__tbeta = zeros(self._tM.shape[1])
 
     def get_fast_scanner(self):
         return FastScanner(self._y, self.M, self._QS, self.delta)
@@ -67,18 +45,12 @@ class LMMCore(object):
         # pylint: disable=W0212
         o = LMMCore.__new__(LMMCore)
         o._fix_scale = self._fix_scale
-        o._diag = [copy(self._diag[i]) for i in [0, 1]]
         o._QS = self._QS
-        # o._M = self._M
         o._y = self._y
 
         o.__tbeta = self.__tbeta.copy()
         o._scale = self._scale
         o._delta = self._delta
-
-        # o._yTQ = self._yTQ
-        # o._yTQ_2x = self._yTQ_2x
-        # o._tMTQ = self._tMTQ
 
         return o
 
@@ -97,11 +69,6 @@ class LMMCore(object):
         self._svd = economic_svd(M)
         self._tM = ddot(self._svd[0], sqrt(self._svd[1]), left=False)
         self.__tbeta = zeros(self._tM.shape[1])
-        # self._M = v
-        # self._covariate_setup(v)
-        # d = self._tM.shape[1]
-        # self.__tbeta = zeros(d)
-        # self._tMTQ = [self._tM.T.dot(self._QS[0][i]) for i in [0, 1]]
 
     @property
     def m(self):
@@ -140,14 +107,20 @@ class LMMCore(object):
     def delta(self, delta):
         self._delta = delta
 
+    def _diag(self, i):
+        if i == 0:
+            return self._QS[1] * (1 - self._delta) + self._delta
+        assert i == 1
+        return self._delta
+
     def _a(self, i):
-        return sum(self._yTQ_2x(i) / self._diag[i])
+        return sum(self._yTQ_2x(i) / self._diag(i))
 
     def _b(self, i):
-        return (self._yTQ(i) / self._diag[i]).dot(self._tMTQ(i).T)
+        return (self._yTQ(i) / self._diag(i)).dot(self._tMTQ(i).T)
 
     def _c(self, i):
-        return (self._tMTQ(i) / self._diag[i]).dot(self._tMTQ(i).T)
+        return (self._tMTQ(i) / self._diag(i)).dot(self._tMTQ(i).T)
 
     def _yTQ(self, i):
         return dot(self._y.T, self._QS[0][i])
@@ -173,14 +146,7 @@ class LMMCore(object):
         p = [a[i] - 2 * b[i].dot(be) + be.dot(c[i]).dot(be) for i in [0, 1]]
         self._scale = sum(p) / len(self._y)
 
-    def _update_diags(self):
-        self._diag[0][:] = self._QS[1]
-        self._diag[0] *= (1 - self._delta)
-        self._diag[0] += self._delta
-        self._diag[1] = self._delta
-
     def update(self):
-        self._update_diags()
         self._update_fixed_effects()
         self._update_scale()
 
@@ -191,6 +157,6 @@ class LMMCore(object):
         p = n - self._QS[1].shape[0]
         LOG2PI = 1.837877066409345339081937709124758839607238769531250
         lml = -n * LOG2PI - n - n * log(self._scale)
-        lml += -sum(log(self._diag[0])) - p * log(self._diag[1])
+        lml += -sum(log(self._diag(0))) - p * log(self._diag(1))
         lml /= 2
         return lml
