ef38aeb11484010013a746eedc8feb89f39af05d
Danilo Horta <danilo.horta@gmail.com>
2017-04-13 19:47:13 +0100

    Low-rank problem

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def setup_package():
 
     metadata = dict(
         name='limix-inference',
-        version='1.1.5',
+        version='1.1.6',
         maintainer="Limix Developers",
         maintainer_email="horta@ebi.ac.uk",
         license="MIT",
