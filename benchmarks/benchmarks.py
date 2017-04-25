8d9056b63860a324bc06a64d5395eb00a44681d1
Danilo Horta <danilo.horta@gmail.com>
2017-04-25 11:28:03 +0100

    Proper benchmark

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,36 +1,35 @@
 from numpy.random import RandomState
 from numpy.testing import assert_allclose
-from numpy import ascontiguousarray, sqrt, ones
+from numpy import ascontiguousarray, sqrt, ones, dot, zeros
 from numpy_sugar.linalg import economic_qs, economic_qs_linear, sum2diag
 
 from glimix_core.example import linear_eye_cov
 from glimix_core.glmm import GLMM
 from glimix_core.random import bernoulli_sample
 
-class TimeSuite:
+class TimeSuite(object):
     def setup(self):
         random = RandomState(0)
         n = 100
-        n1k = 1000
-        self._X = random.randn(n, 5)
-        self._K = linear_eye_cov().feed().value()
-        self._QS = economic_qs(self._K)
+
+        self._X = ones((n, 1))
+        self._G = random.randn(n, n + 5)
+        K = dot(self._G, self._G.T)
+        K /= K.diagonal().mean()
+        sum2diag(K, 1e-3, out=K)
+        self._QS = economic_qs(K)
 
         self._ntri = random.randint(1, 30, n)
-        self._nsuc = [random.randint(0, i) for i in self._ntri]
 
-        self._X1k = random.randn(n1k, 5)
-        self._K1k = self._X1k.dot(self._X1k.T)
-        self._K1k /= self._K1k.diagonal().mean()
-        sum2diag(self._K1k, 1e-3, out=self._K1k)
-        self._QS1k = economic_qs(self._K1k)
+        z = dot(self._G, random.randn(n + 5)) / sqrt(n + 5)
 
-        self._ntri1k = random.randint(1, 30, n1k)
-        self._nsuc1k = [random.randint(0, i) for i in self._ntri1k]
+        self._nsuc = zeros(len(self._ntri), int)
+        for i in range(len(self._ntri)):
+            for j in range(self._ntri[i]):
+                self._nsuc[i] += int(z[i] + 0.5 * random.randn() > 0)
 
     def time_qep_binomial_1k_learn(self):
-        glmm = GLMM((self._nsuc1k, self._ntri1k), 'binomial', self._X1k,
-                    self._QS1k)
+        glmm = GLMM((self._nsuc, self._ntri), 'binomial', self._X,
+                    self._QS)
         glmm.feed().maximize(progress=False)
-        print(".12f" % glmm.feed().value())
-        assert_allclose(glmm.feed().value(), -2611.6160207784023)
+        assert_allclose(glmm.feed().value(), -218.95038533366554)
