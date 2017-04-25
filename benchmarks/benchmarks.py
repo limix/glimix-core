35b59a0046725ee420dc0bc02dae32a1daba161a
Danilo Horta <danilo.horta@gmail.com>
2017-04-25 12:57:38 +0100

    Bench

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -7,29 +7,41 @@ from glimix_core.example import linear_eye_cov
 from glimix_core.glmm import GLMM
 from glimix_core.random import bernoulli_sample
 
+def get_data(n):
+    random = RandomState(0)
+
+    X = ones((n, 1))
+    G = random.randn(n, n + 5)
+    K = dot(G, G.T)
+    K /= K.diagonal().mean()
+    sum2diag(K, 1e-3, out=K)
+    QS = economic_qs(K)
+
+    ntri = random.randint(1, 30, n)
+
+    z = dot(G, random.randn(n + 5)) / sqrt(n + 5)
+
+    nsuc = zeros(len(ntri), int)
+    for i in range(len(ntri)):
+        for j in range(ntri[i]):
+            nsuc[i] += int(z[i] + 0.5 * random.randn() > 0)
+
+    return (ntri, nsuc, QS, X)
+
 class TimeSuite(object):
     def setup(self):
-        random = RandomState(0)
-        n = 10
-
-        self._X = ones((n, 1))
-        self._G = random.randn(n, n + 5)
-        K = dot(self._G, self._G.T)
-        K /= K.diagonal().mean()
-        sum2diag(K, 1e-3, out=K)
-        self._QS = economic_qs(K)
-
-        self._ntri = random.randint(1, 30, n)
-
-        z = dot(self._G, random.randn(n + 5)) / sqrt(n + 5)
-
-        self._nsuc = zeros(len(self._ntri), int)
-        for i in range(len(self._ntri)):
-            for j in range(self._ntri[i]):
-                self._nsuc[i] += int(z[i] + 0.5 * random.randn() > 0)
+        self._data = {10: get_data(10), 100: get_data(100)}
 
     def time_qep_binomial_10_learn(self):
-        glmm = GLMM((self._nsuc, self._ntri), 'binomial', self._X,
-                    self._QS)
+        (ntri, nsuc, QS, X) = self._data[10]
+
+        glmm = GLMM((nsuc, ntri), 'binomial', X, QS)
         glmm.feed().maximize(progress=False)
         assert_allclose(glmm.feed().value(), -19.74077399053363)
+
+    def time_qep_binomial_100_learn(self):
+        (ntri, nsuc, QS, X) = self._data[100]
+
+        glmm = GLMM((nsuc, ntri), 'binomial', X, QS)
+        glmm.feed().maximize(progress=False)
+        assert_allclose(glmm.feed().value(), -218.9503853656612)
