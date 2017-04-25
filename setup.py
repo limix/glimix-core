a4d80ea97324c92ba47a3db1d40be7a544159950
Danilo Horta <danilo.horta@gmail.com>
2017-04-25 11:12:37 +0100

    Bump

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def setup_package():
 
     metadata = dict(
         name='glimix-core',
-        version='1.2.1',
+        version='1.2.2',
         maintainer="Limix Developers",
         maintainer_email="horta@ebi.ac.uk",
         license="MIT",
