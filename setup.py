a53a1441e9e2b4f3e97564dbcf90d10930f9d123
Danilo Horta <danilo.horta@gmail.com>
2017-08-14 11:05:34 +0100

    optimix 1.2.14

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def setup_package():
     setup_requires = ['ncephes>=1.0.21'] + pytest_runner
     install_requires = [
         'scipy>=0.18', 'ncephes>=1.0.40', 'numpy>=1.10', 'numpy-sugar>=1.0.47',
-        'scipy-sugar>=1.0.2', 'optimix>=1.2.13', 'cachetools>=2.0', 'tqdm>=4',
+        'scipy-sugar>=1.0.2', 'optimix>=1.2.14', 'cachetools>=2.0', 'tqdm>=4',
         'liknorm-py>=1.0.6'
     ]
     tests_require = ['pytest', 'pytest-pep8']
