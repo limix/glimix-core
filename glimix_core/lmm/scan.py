48ed6e018f10a94504cf3cba930743c4b5a2eb0b
Danilo Horta <danilo.horta@gmail.com>
2017-09-25 15:01:05 +0100

    sane var name, fastscan

--- glimix_core/lmm/scan.py
+++ glimix_core/lmm/scan.py
@@ -13,6 +13,8 @@ from numpy_sugar import epsilon
 from numpy_sugar.linalg import rsolve, solve
 from tqdm import tqdm
 
+from ..util import log2pi
+
 
 class FastScanner(object):
     r"""Approximated fast inference over several covariates.
@@ -90,7 +92,7 @@ class FastScanner(object):
 
     Attributes
     ----------
-    _a : tuple
+    _yTQDiQTy : tuple
         Store the arrays
 
         .. math::
@@ -101,7 +103,7 @@ class FastScanner(object):
 
         for :math:`i = \{0, 1\}`.
 
-    _b : tuple
+    _yTQDiQTX : tuple
         Store the arrays
 
         .. math::
@@ -112,12 +114,12 @@ class FastScanner(object):
 
         for :math:`i = \{0, 1\}`.
 
-    _yTQdiag : tuple
+    _yTQDi : tuple
         Store the arrays
         :math:`(\mathrm Q_i^{\intercal} \mathbf y)^{\intercal}\mathrm D_i^{-1}`
         , for :math:`i = \{0, 1\}`.
 
-    _XTQdiag : tuple
+    _XTQDi : tuple
         Store the arrays
         :math:`(\mathrm Q_i^{\intercal} \mathrm X)^{\intercal}\mathrm D_i^{-1}`
         , for :math:`i = \{0, 1\}`.
@@ -144,26 +146,27 @@ class FastScanner(object):
 
         XTQ = [dot(X.T, Q) for Q in QS[0]]
 
-        self._yTQdiag = [l / r for (l, r) in zip(yTQ, self._D)]
+        self._yTQDi = [l / r for (l, r) in zip(yTQ, self._D)]
 
-        self._a = [(i**2 / j).sum() for (i, j) in zip(yTQ, self._D)]
+        self._yTQDiQTy = [(i**2 / j).sum() for (i, j) in zip(yTQ, self._D)]
 
-        self._b = [dot(i, j.T) for (i, j) in zip(self._yTQdiag, XTQ)]
+        self._yTQDiQTX = [dot(i, j.T) for (i, j) in zip(self._yTQDi, XTQ)]
 
-        self._XTQdiag = [i / j for (i, j) in zip(XTQ, self._D)]
+        self._XTQDi = [i / j for (i, j) in zip(XTQ, self._D)]
 
         nc = X.shape[1]
 
         self._C = [empty((nc + 1, nc + 1)), empty((nc + 1, nc + 1))]
 
         for i in range(2):
-            self._C[i][:-1, :-1] = dot(self._XTQdiag[i], XTQ[i].T)
+            self._C[i][:-1, :-1] = dot(self._XTQDi[i], XTQ[i].T)
+
+        self._XTQDiQTX = [self._C[i][:-1, :-1] for i in range(2)]
 
     def _static_lml(self):
-        LOG2PI = 1.837877066409345339081937709124758839607238769531250
         n = self._QS[0][0].shape[0]
         p = len(self._D[0])
-        static_lml = -n * LOG2PI - n
+        static_lml = -n * log2pi - n
         static_lml -= npsum(log(self._D[0]))
         static_lml -= (n - p) * log(self._D[1])
         return static_lml
@@ -178,10 +181,10 @@ class FastScanner(object):
             raise ValueError("One or more variants have non-finite value.")
 
         mTQ = [dot(markers.T, Q) for Q in self._QS[0]]
-        bm = [dot(i, j.T) for (i, j) in zip(self._yTQdiag, mTQ)]
+        yTQDiQTm = [dot(i, j.T) for (i, j) in zip(self._yTQDi, mTQ)]
 
-        c_01 = [dot(i, j.T) for (i, j) in zip(self._XTQdiag, mTQ)]
-        c_11 = [npsum((i / j) * i, axis=1) for (i, j) in zip(mTQ, self._D)]
+        XTQDiQTm = [dot(i, j.T) for (i, j) in zip(self._XTQDi, mTQ)]
+        mTQDiQTm = [npsum((i / j) * i, axis=1) for (i, j) in zip(mTQ, self._D)]
 
         nsamples = markers.shape[0]
         nmarkers = markers.shape[1]
@@ -189,34 +192,36 @@ class FastScanner(object):
         lmls = full(nmarkers, self._static_lml())
         effect_sizes = empty(nmarkers)
 
-        if self._XTQdiag[0].shape[0] == 1:
+        if self._XTQDi[0].shape[0] == 1:
             return self._fast_scan_chunk_1covariate_loop(
-                lmls, effect_sizes, bm, c_01, c_11, nsamples)
+                lmls, effect_sizes, yTQDiQTm, XTQDiQTm, mTQDiQTm, nsamples)
         else:
             return self._fast_scan_chunk_multicovariate_loop(
-                lmls, effect_sizes, bm, c_01, c_11, nsamples, nmarkers)
+                lmls, effect_sizes, yTQDiQTm, XTQDiQTm, mTQDiQTm, nsamples,
+                nmarkers)
 
-    def _fast_scan_chunk_multicovariate_loop(self, lmls, effect_sizes, bm,
-                                             c_01, c_11, nsamples, nmarkers):
-        b00m = empty(len(self._b[0]) + 1)
-        b00m[:-1] = self._b[0]
+    def _fast_scan_chunk_multicovariate_loop(self, lmls, effect_sizes,
+                                             yTQDiQTm, XTQDiQTm, mTQDiQTm,
+                                             nsamples, nmarkers):
+        b00m = empty(len(self._yTQDiQTX[0]) + 1)
+        b00m[:-1] = self._yTQDiQTX[0]
 
-        b11m = empty(len(self._b[1]) + 1)
-        b11m[:-1] = self._b[1]
+        b11m = empty(len(self._yTQDiQTX[1]) + 1)
+        b11m[:-1] = self._yTQDiQTX[1]
 
         for i in range(nmarkers):
 
-            b00m[-1] = bm[0][i]
-            b11m[-1] = bm[1][i]
+            b00m[-1] = yTQDiQTm[0][i]
+            b11m[-1] = yTQDiQTm[1][i]
 
-            self._C[0][:-1, -1] = c_01[0][:, i]
-            self._C[1][:-1, -1] = c_01[1][:, i]
+            self._C[0][:-1, -1] = XTQDiQTm[0][:, i]
+            self._C[1][:-1, -1] = XTQDiQTm[1][:, i]
 
             self._C[0][-1, :-1] = self._C[0][:-1, -1]
             self._C[1][-1, :-1] = self._C[1][:-1, -1]
 
-            self._C[0][-1, -1] = c_11[0][i]
-            self._C[1][-1, -1] = c_11[1][i]
+            self._C[0][-1, -1] = mTQDiQTm[0][i]
+            self._C[1][-1, -1] = mTQDiQTm[1][i]
 
             try:
                 beta = solve(self._C[1] - self._C[0], b11m - b00m)
@@ -233,9 +238,13 @@ class FastScanner(object):
             effect_sizes[i] = beta[-1]
 
             if self._scale is None:
-                p0 = self._a[0] - 2 * b00m.dot(beta) + beta.dot(
+                # _compute_scale(nsamples, beta, self._yTQDiQTy, self._yTQDiQTX, )
+                # (nsamples, beta, yTQDiQTy, yTQDiQTX, yTQDiQTm, XTQDiQTX,
+                #                    XTQDiQTm, mTQDiQTm)
+
+                p0 = self._yTQDiQTy[0] - 2 * b00m.dot(beta) + beta.dot(
                     self._C[0]).dot(beta)
-                p1 = self._a[1] - 2 * b11m.dot(beta) + beta.dot(
+                p1 = self._yTQDiQTy[1] - 2 * b11m.dot(beta) + beta.dot(
                     self._C[1].dot(beta))
 
                 scale = (p0 + p1) / nsamples
@@ -247,20 +256,20 @@ class FastScanner(object):
         lmls /= 2
         return lmls, effect_sizes
 
-    def _fast_scan_chunk_1covariate_loop(self, lmls, effect_sizes, bm, c_01,
-                                         C11, nsamples):
+    def _fast_scan_chunk_1covariate_loop(self, lmls, effect_sizes, yTQDiQTm,
+                                         XTQDiQTm, mTQDiQTm, nsamples):
 
         C00 = [C[0, 0] for C in self._C]
-        C01 = [c[0, :] for c in c_01]
+        C01 = [c[0, :] for c in XTQDiQTm]
 
-        b = [bi[0] for bi in self._b]
+        b = [bi[0] for bi in self._yTQDiQTX]
 
         sC00 = C00[1] - C00[0]
         sC01 = C01[1] - C01[0]
-        sC11 = C11[1] - C11[0]
+        sC11 = mTQDiQTm[1] - mTQDiQTm[0]
 
         sb = b[1] - b[0]
-        sbm = bm[1] - bm[0]
+        sbm = yTQDiQTm[1] - yTQDiQTm[0]
 
         with errstate(divide='ignore'):
             beta = beta_1covariate(sb, sbm, sC00, sC01, sC11)
@@ -271,9 +280,10 @@ class FastScanner(object):
 
         if self._scale is None:
             for i in range(2):
-                scale += self._a[i] - 2 * (b[i] * beta[0] + bm[i] * beta[1])
+                scale += self._yTQDiQTy[i] - 2 * (
+                    b[i] * beta[0] + yTQDiQTm[i] * beta[1])
                 scale += beta[0] * (C00[i] * beta[0] + C01[i] * beta[1])
-                scale += beta[1] * (C01[i] * beta[0] + C11[i] * beta[1])
+                scale += beta[1] * (C01[i] * beta[0] + mTQDiQTm[i] * beta[1])
             scale /= nsamples
         else:
             scale = self._scale
@@ -340,29 +350,20 @@ class FastScanner(object):
 
             - \frac{n}{2}\log{2\pi}
                 - \frac{1}{2} n \log{s}
-                - \frac{1}{2} \log{\left|\mathrm K + v \mathrm I \right|}
+                - \frac{1}{2} \log{\left|\mathrm D\right|}
                     - \frac{1}{2}
-                    \left(\mathbf y -
-                    \mathrm X\boldsymbol\beta\right)^{\intercal}
-                    s^{-1}\left(\mathrm K + v \mathrm I \right)^{-1}
-                    \left(\mathbf y -
-                    \mathrm X\boldsymbol\beta\right)
+                \left(\mathrm Q^{\intercal}\mathbf y -
+              \mathrm Q^{\intercal}\mathrm X\boldsymbol\beta\right)^{\intercal}
+                    s^{-1}\mathrm D^{-1}
+                \left(\mathrm Q^{\intercal}\mathbf y -
+                \mathrm Q^{\intercal}\mathrm X\boldsymbol\beta\right)
         """
-        # TODO
-        # def _diag(self, i):
-        #     if i == 0:
-        #         return self._QS[1] * (1 - self.delta) + self.delta
-        #     return self.delta
-        LOG2PI = 1.837877066409345339081937709124758839607238769531250
         n = self._QS[0][0].shape[0]
-        p = n - self._QS[1].shape[0]
-        if self._scale is None:
-            pass
-        else:
-            lml = -n * LOG2PI - n - n * log(self._scale)
-        lml += -sum(log(self._D[0])) - p * log(self._D[1])
-        lml /= 2
-        return lml
+        p = len(self._D[0])
+        static_lml = -n * log2pi - n
+        static_lml -= npsum(log(self._D[0]))
+        static_lml -= (n - p) * log(self._D[1])
+        return static_lml
 
     def set_scale(self, scale):
         r"""Set the scaling factor.
@@ -395,3 +396,18 @@ def beta_1covariate(sb, sbm, sC00, sC01, sC11):
     d6 = sC11 / sC01
 
     return [(d1 - d4) / (d5 - 1 / d6), (-d0 + d3) / (d6 - 1 / d5)]
+
+
+def _compute_scale(nsamples, beta, yTQDiQTy, yTQDiQTX, yTQDiQTm, XTQDiQTX,
+                   XTQDiQTm, mTQDiQTm):
+
+    scale = npsum(yTQDiQTy[i] for i in range(2))
+    scale -= npsum(2 * yTQDiQTX[i] * beta[0] for i in range(2))
+    scale -= npsum(2 * yTQDiQTm[i] * beta[1] for i in range(2))
+
+    scale += npsum(beta[0] * XTQDiQTX[i] * beta[0] for i in range(2))
+    scale += npsum(beta[0] * XTQDiQTm[i] * beta[1] for i in range(2))
+    scale += npsum(beta[1] * XTQDiQTm[i] * beta[0] for i in range(2))
+    scale += npsum(beta[1] * mTQDiQTm[i] * beta[1] for i in range(2))
+
+    return scale / nsamples
