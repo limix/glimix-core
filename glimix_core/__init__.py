ba04087a2ecc1ac0ff810c605113a56784b90b69
Danilo Horta <danilo.horta@gmail.com>
2018-01-25 14:39:38 +0000

    before setup changes

--- glimix_core/__init__.py
+++ glimix_core/__init__.py
@@ -10,14 +10,12 @@ Fast inference for Generalised Linear Mixed Models.
 from __future__ import absolute_import as _
 
 from . import cov, ggp, glmm, gp, lik, link, lmm, mean, random, util
+from .__about__ import (__name__, __version__, __author__, __author_email__,
+                        __maintainer__, __maintainer_email__, __description__)
 from .testit import test
 
-__name__ = "glimix-core"
-__version__ = "1.3.7"
-__author__ = "Danilo Horta"
-__author_email__ = "horta@ebi.ac.uk"
-
 __all__ = [
-    "__name__", "__version__", "__author__", "__author_email__", "test", 'ggp',
-    'gp', 'lmm', 'glmm', 'cov', 'lik', 'mean', 'link', 'random', 'util'
+    "__name__", "__version__", "__author__", "__author_email__",
+    "__maintainer__", "__maintainer_email__", "__description__", "test", "ggp",
+    "gp", "lmm", "glmm", "cov", "lik", "mean", "link", "random", "util"
 ]
