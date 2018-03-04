0f31e771bdcbe676483068db57348fe0ea279f0f
Danilo Horta <danilo.horta@gmail.com>
2018-03-04 13:44:14 +0000

    1k

--- speed_1k.py
+++ speed_1k.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
 
     nsamples = 1000
 
-    random = np.random.RandomState(1)
+    random = np.random.RandomState(2)
     X = random.randn(nsamples, nsamples + 1)
     X -= X.mean(0)
     X /= X.std(0)
@@ -15,25 +15,30 @@ if __name__ == '__main__':
 
     K = X.dot(X.T)
 
-    z = random.multivariate_normal(0.2 * np.ones(nsamples), K)
+    z = random.multivariate_normal(0.2 * np.ones(nsamples), 0.2 * K)
     QS = ns.linalg.economic_qs(K)
 
     ntri = random.randint(5, 30, nsamples)
     nsuc = np.zeros(nsamples, dtype=int)
 
     for (i, ni) in enumerate(ntri):
-        nsuc[i] += sum(z[i] + 0.2 * random.randn(ni) > 0)
+        nsuc[i] += sum(z[i] + 0.4 * random.randn(ni) > 0)
 
     ntri = np.ascontiguousarray(ntri)
 
-    glmm = GLMMExpFam(
-        (nsuc, ntri),
-        'binomial',
-        X,
-        QS,
-        n_int=200,
-        rtol=ns.epsilon.small * 1000 * 100,
-        atol=ns.epsilon.small * 100)
+    # glmm = GLMMExpFam(
+    #     (nsuc, ntri),
+    #     'binomial',
+    #     np.ones((nsamples, 1)),
+    #     QS,
+    #     n_int=200,
+    #     rtol=ns.epsilon.small * 1000 * 100,
+    #     atol=ns.epsilon.small * 100)
+    glmm = GLMMExpFam((nsuc, ntri), 'binomial', np.ones((nsamples, 1)), QS)
     glmm.fit(verbose=True, factr=1e6, pgtol=1e-3)
     print(glmm.lml())
-    # -373.2819316198989
+    print(glmm.scale)
+    print(glmm.delta)
+    # -2483.923312970759
+    # 4.211424061421387
+    # 0.013175939427293077
