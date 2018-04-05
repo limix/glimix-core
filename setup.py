747b16bb561174695db5d12d6240c5f5fce3aa06
Danilo Horta <danilo.horta@gmail.com>
2018-04-05 20:15:00 +0100

    content type markdown

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
 from setuptools import setup
 
 if __name__ == '__main__':
-    readme = open('README.md').read()
-    try:
-        import pypandoc
-        long_description = pypandoc.convert_text(
-            readme, 'rst', format='markdown')
-    except (ImportError, RuntimeError, OSError):
-        long_description = readme
-
-    setup(long_description=long_description)
+    setup()
