df3ed1d67c74e4ab5bf8902611fd96e3a027d5ca
Danilo Horta <danilo.horta@gmail.com>
2018-07-16 20:11:10 +0100

    1.5.3

--- glimix_core/__init__.py
+++ glimix_core/__init__.py
@@ -12,7 +12,7 @@ from __future__ import absolute_import
 from . import cov, example, ggp, glmm, gp, lik, link, lmm, mean, random
 from ._testit import test
 
-__version__ = "1.5.2"
+__version__ = "1.5.3"
 
 __all__ = [
     "__version__",
