23113744dbdf52775f5b68cf7cbe72d603150998
Danilo Horta <danilo.horta@gmail.com>
2019-02-26 19:48:10 +0000

    save

--- glimix_core/lmm/test/test_lmm_mt.py
+++ glimix_core/lmm/test/test_lmm_mt.py
@@ -1,5 +1,3 @@
-from __future__ import division
-
 import pytest
 from numpy import arange, concatenate, inf, nan, ones, sqrt, zeros
 from numpy.random import RandomState
@@ -352,3 +350,66 @@ def _outcome_sample(random, offset, X):
     lik = DeltaProdLik()
 
     return GGPSampler(lik, mean, cov).sample(random)
+
+
+def test_lmm_mt_lmm_interface():
+    random = RandomState(0)
+    y0 = random.randn(4)
+    y1 = random.randn(5)
+    X = random.randn(5, 2)
+    G = random.randn(5, 6)
+
+    QS = economic_qs_linear(G)
+    with pytest.raises(ValueError):
+        MTLMM([y0, y1], X, QS)
+
+    with pytest.raises(ValueError):
+        MTLMM([y1, y0], X, QS)
+
+    y0 = random.randn(5)
+
+    with pytest.raises(ValueError):
+        MTLMM([y1, y0], X, QS)
+
+    X0 = random.randn(5, 2)
+    X1 = random.randn(5, 3)
+
+    G1 = random.randn(6, 6)
+    QS1 = economic_qs_linear(G1)
+
+    with pytest.raises(ValueError):
+        MTLMM([y1, y0], [X0, X1], QS1)
+
+    lmm = MTLMM([y0, y1], [X0, X1], QS)
+    lml = lmm.lml()
+
+    assert_allclose(lml, -7.713526905947342)
+
+    lmm = MTLMM(y0, X0, QS)
+    lml = lmm.lml()
+    assert_allclose(lml, -5.721633217309584)
+
+    y0[3] = inf
+    with pytest.raises(ValueError):
+        MTLMM([y1, y0], [X0, X1], QS1)
+
+    y0[3] = nan
+    with pytest.raises(ValueError):
+        MTLMM([y1, y0], [X0, X1], QS1)
+
+
+def test_lmm_mt_lmm_pandas():
+    from pandas import Series, DataFrame
+
+    random = RandomState(0)
+    y = Series(random.randn(5))
+    X = DataFrame(random.randn(5, 2))
+    G = random.randn(5, 6)
+
+    QS = economic_qs_linear(G)
+
+    lmm = MTLMM(y, X, QS)
+    assert_allclose(lmm.lml(), -6.7985689491)
+
+    lmm = MTLMM([y, y], [X, X], QS)
+    assert_allclose(lmm.lml(), -10.474952610114517)
