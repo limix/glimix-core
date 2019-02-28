b00e80271f62256ee92542929682da1221c4e534
Danilo Horta <danilo.horta@gmail.com>
2019-02-28 22:19:58 +0000

    before L0L1 concat

--- glimix_core/cov/test/test_cov_free.py
+++ glimix_core/cov/test/test_cov_free.py
@@ -5,6 +5,14 @@ from glimix_core.cov import FreeFormCov
 
 
 def test_freeformcov():
+
+    cov = FreeFormCov(3)
+    L = array([[1.5, 0, 0], [2.5, 0.8, 0], [-0.3, 0.4, 2.1]], float)
+    cov.L = L
+    assert_allclose(cov.value(), L @ L.T, rtol=1e-4)
+    assert_allclose(cov._check_grad(), 0, atol=1e-5)
+    assert_allclose(cov.logdet(), 1.8486202109176444)
+
     cov = FreeFormCov(2)
     L = array([[1, 0], [2.5, 1]], float)
     cov.L = L
