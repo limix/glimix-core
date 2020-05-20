d486a149ec62d0a14d5e297c89f2da5dc069ba1f
Danilo Horta <danilo.horta@pm.me>
2020-05-20 15:55:29 +0100

    svd util, LMM using Q0 only, except for FastScanner

--- glimix_core/_util/__init__.py
+++ glimix_core/_util/__init__.py
@@ -2,6 +2,7 @@ from . import _numbers as numbers
 from ._array import unvec, vec
 from ._assert import assert_interface
 from ._numbers import safe_log
+from ._svd import SVD
 from .cache import cache
 from .check import check_covariates, check_economic_qs, check_outcome
 from .eigen import economic_qs_zeros
@@ -12,6 +13,7 @@ from .solve import force_inv, hinv, hsolve, rsolve
 log2pi = 1.837877066409345339081937709124758839607238769531250
 
 __all__ = [
+    "SVD",
     "assert_interface",
     "cache",
     "check_covariates",
---
+++ glimix_core/_util/_svd.py
@@ -0,0 +1,64 @@
+from numpy_sugar.linalg import ddot, economic_svd
+
+
+class SVD:
+    """
+    Represents a SVD decomposition with some useful methods.
+
+    Let 𝙰 be a given 𝑚×𝑛 real matrix. The SVD decomposition of 𝙰 is defined by ::
+
+        𝙰 = 𝚄𝚂𝚅ᵀ
+
+    for which 𝚂 is a diagonal matrix.
+
+    Parameters
+    ----------
+    A
+        Matrix 𝙰. Optional if the parameter `USVt` is given.
+    USVt
+        SVD decomposition of 𝙰. Optional if the parameter `A` is given.
+    """
+
+    def __init__(self, A=None, USVt=None):
+        if A is None and USVt is None:
+            raise ValueError("Both `A` and `USVt` cannot be `None`.")
+
+        if A is None:
+            self._US = ddot(USVt[0], USVt[1])
+            self._Vt = USVt[2]
+            self._A = self._US @ self._Vt
+        else:
+            USVt = economic_svd(A)
+            self._US = ddot(USVt[0], USVt[1])
+            self._Vt = USVt[2]
+            self._A = A
+
+        self._rank = len(USVt[1])
+
+    @property
+    def A(self):
+        """
+        Get 𝙰.
+        """
+        return self._A
+
+    @property
+    def US(self):
+        """
+        Get 𝚄𝚂.
+        """
+        return self._US
+
+    @property
+    def Vt(self):
+        """
+        Get 𝚅ᵀ.
+        """
+        return self._Vt
+
+    @property
+    def rank(self):
+        """
+        Get the diagonal size of matrix 𝚂.
+        """
+        return self._rank
