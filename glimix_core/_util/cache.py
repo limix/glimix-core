0f96303b5e985502b3adb70c7f6c94dcb087eb95
Danilo Horta <danilo.horta@pm.me>
2021-05-17 21:04:01 +0100

    from functools import cached_property

--- glimix_core/_util/cache.py
+++ glimix_core/_util/cache.py
@@ -6,8 +6,4 @@ __all__ = ["cached_property"]
 if sys.version_info < (3, 8):
     from .cached_property import cached_property
 else:
-
-    def cached_property(func):
-        from functools import lru_cache
-
-        return property(lru_cache(maxsize=None)(func))
+    from functools import cached_property
